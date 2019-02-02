8047cd61622373d2a82fd3466a973178b39ff262
Thomas Hunter <th@thomashunter.co.uk>
2019-02-02 21:17:41 +0000

    Made webview configurable

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { WebView } from 'react-native-webview';
+import Config from 'react-native-config';
 
 class App extends Component {
     render() {
         return (
             <WebView
-                source={{ uri: "https://www.google.com" }}
+                source={{ uri: Config.WEBVIEW_LOCATION }}
                 style={{ marginTop: 20 }}
                 onLoadProgress={e => console.log(e.nativeEvent.progress)}
             />
